9dc2832bbb00f5a29e95fb6eb87e75c2c46eef3c
Stefano Fornari <stefano.fornari@gmail.com>
2011-12-29 17:57:49 +0100

    Added support for controllers and views prefixes

--- src/main/java/ste/web/beanshell/BeanShellServlet.java
+++ src/main/java/ste/web/beanshell/BeanShellServlet.java
@@ -27,18 +27,89 @@ import javax.servlet.http.*;
 import bsh.*;
 import java.util.logging.Level;
 
+/**
+ * Executes the bsh script specified by the URL. The script is the controller
+ * and shall set the view that should be rendered after the execution of the 
+ * script in the bsh variable <i>view</i>.
+ * 
+ * The lookup of the scripts and views are controlled by the following pattern:
+ * <pre>
+ *   {context}{controllers-prefix}/{script-pathname}
+ *   {context}{vies-prefix}/{jsp-pathname}
+ * </pre>
+ * For example, if context=/myapp, controllers-prefix=c and views-prefix=v,
+ * the URL http://myserver:8080/myapp/mycontroller.bsh will read the script
+ * {CONTAINER_HOME}/myapp/c/mycontroller.bsh. If the controller sets <i>view</i>
+ * to myview.jsp, the request is forwarded to http://myserver:8080/myapp/v/myview.jsp
+ * 
+ * <i>controllers-prefix</i> and <i>views-prefix</i> can be set as context 
+ * parameters in web.xml:
+ * 
+ * &lt;context-param&gt;
+ *   &lt;param-name&gt;<b>controllers-prefix</b>&lt;/param-name&gt;
+ *   &lt;param-value&gt;<b>controllers</b>&lt;/param-value&gt;
+ * &lt;/context-param&gt;
+ * &lt;context-param&gt;
+ *   &lt;param-name&gt;<b>views-prefix</b>&lt;/param-name&gt;
+ *   &lt;param-value&gt;<b>views</b>&lt;/param-value&gt;
+ * &lt;/context-param&gt;
+ * 
+ * <i>controllers-prefix</i> and <i>views-prefix</i> defauult to "".
+ * 
+ * @author ste
+ */
 public class BeanShellServlet
 extends HttpServlet {
     // --------------------------------------------------------------- Constants
 
     public static final String LOG_NAME = "ste.web";
+    
+    public static final String PARAM_CONTROLLERS          = "controllers-prefix";
+    public static final String PARAM_VIEWS                = "views-prefix"      ;
+    public static final String DEFAULT_CONTROLLERS_PREFIX = "/"                 ;
+    public static final String DEFAULT_VIEWS_PREFIX       = "/"                 ;
 
     // ------------------------------------------------------------ Private data
 
     private static final Logger log = Logger.getLogger(LOG_NAME);
+    
+    private static String controllersPrefix = "controllers";
+    private static String viewsPrefix       = "views"      ;
 
     // ---------------------------------------------------------- Public methods
-    public void init() throws ServletException {
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+
+        ServletContext context = getServletContext();
+        
+        controllersPrefix = context.getInitParameter(PARAM_CONTROLLERS);
+        if (controllersPrefix == null) {
+            controllersPrefix = context.getInitParameter(DEFAULT_CONTROLLERS_PREFIX);
+        } else {
+            //
+            // let's fix a common mistake :)
+            //
+            if (!controllersPrefix.startsWith("/")) {
+                controllersPrefix = '/' + controllersPrefix;
+            }
+        }
+        
+        viewsPrefix = context.getInitParameter(PARAM_VIEWS);
+        if (viewsPrefix == null) {
+            context.getInitParameter(DEFAULT_VIEWS_PREFIX);
+        }  else {
+            //
+            // let's fix a common mistake :)
+            //
+            if (!viewsPrefix.endsWith("/")) {
+                viewsPrefix = viewsPrefix + '/';
+            }
+        }
+        
+        if (log.isLoggable(Level.FINE)) {
+            log.fine("controllers-prefix: " + controllersPrefix);
+            log.fine("views-prefix: " + viewsPrefix);
+        }
     }
 
     public void doGet(final HttpServletRequest  request ,
@@ -67,14 +138,15 @@ extends HttpServlet {
 
             interpreter.eval(getScript(request));
             
-
-            Object nextView = interpreter.get("view");
-
+            String nextView = (String)interpreter.get("view");
             if ((nextView != null) && (nextView instanceof String)) {
+                nextView = viewsPrefix + nextView;
+                
                 if (log.isLoggable(Level.FINE)) {
                     log.fine("Forwarding to " + nextView);
                 }
-                request.getRequestDispatcher((String)nextView).forward(request, response);
+                
+                request.getRequestDispatcher(nextView).forward(request, response);
             }
         } catch (Exception e) {
             handleError(request, response, e);
@@ -103,9 +175,10 @@ extends HttpServlet {
         interpreter.set("log"     , log                       );
 
         //
-        // Import common commands
+        // Import common commands... I am commenting it out for now; I will 
+        // reintroduce it if and when I will add the helpers
         //
-        interpreter.eval("importCommands(\"commands\");");
+        //interpreter.eval("importCommands(\"commands\");");
 
         return interpreter;
     }
@@ -114,17 +187,19 @@ extends HttpServlet {
     throws IOException {
         String script     = request.getServletPath();
         File   scriptFile = new File(request.getSession().getServletContext().getRealPath(script));
-
+        String controllerPath = scriptFile.getParent() + controllersPrefix;
+        File   controllerFile = new File(controllerPath, scriptFile.getName());
+        
         if (log.isLoggable(Level.FINE)) {
             log.fine("script: " + script);
-            log.fine("file: " + scriptFile);
+            log.fine("controllerFile: " + controllerFile);
         }
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         FileInputStream       is   = null;
 
         try {
-            is = new FileInputStream(scriptFile);
+            is = new FileInputStream(controllerFile);
 
             if (is == null) {
                 throw new FileNotFoundException(script);
